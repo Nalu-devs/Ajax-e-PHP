f8f17f24828b19767dd12a44fa792adf99d33a5d
Nalu <pordosol699@gmail.com>
2025-07-15 10:53:24 -0300

    operadores logicos, aritmeticos e outros

---
+++ PHP/operadores.php
@@ -0,0 +1,59 @@
+<?php
+
+//ARITMETICOS:
+//+ adição
+//- subtração
+//* multiplicação
+// / divisão
+//% resto da divisão
+
+//ARITMETICOS EM UM OPERADOR:
+// -var: troca o sinal do operador
+// ++var: pre-incremento, primeiro incrementa depois realiza a operação
+// --var: pre-decremento, primeiro decrementa depois realiza a operação
+// var++: pos-incremento, primeiro  realiza a operação depois incrementa
+// var--: pos-decremento, primeiro  realiza a operação depois decrementa
+
+
+//BINARIOS:
+// ~op1: inverte os bits de op1
+// op1 & op2: operação E (AND) bit a bit
+// op1 | op2: operação OU (OR) bit a bit
+// op1 ^ op2: operação OU exclusivo (XOR)
+// op1 >> n: desloca op1 n bits à direita
+// op1 << n: desloca op1 n bits à esquerda
+
+//COMPARAÇÃO:
+// op1 == op2: verdadeiro se op1 for igual a op2
+// op1 === op2: verdadeiro se op1 for igual a op2 e eles forem do mesmo tipo
+// op1 >= op2: verdadeiro se op1 for maior ou igual a op2
+// op1 <= op2: verdadeiro se op1 for menor ou igual a op2
+// op1 != op2: verdadeiro se op1 for diferente de op2
+// op1 !== op2: verdadeiro se op1 for diferente de op2 ou eles não forem do mesmo tipo
+// op1 <> op2: tambem serve para representar diferença
+// op1 > op2: verdadeiro se op1 for maior que op2
+// op1 < op2: verdadeiro se op1 for menor que op2
+
+//ATRIBUIÇÃO
+//op1 = op2: op1 recebe o valor de op2
+//op1 += op2: mesmo que op1=op1+op2
+//op1 -= op2: mesmo que op1=op1-op2
+//op1 *= op2: mesmo que op1=op1*op2
+//op1 /= op2: mesmo que op1=op1/op2
+//op1 .= op2: concatenação mesmo que op1=op1.op2
+//op1 %= op2: mesmo que op1=op1%op2
+//op1 <<= op2: mesmo que op1=op1<<op2
+//op1 >>= op2: mesmo que op1=op1>>op2
+//op1 &= op2: mesmo que op1=op1&op2
+//op1 |= op2: mesmo que op1=op1|op2
+//op1 ^= op2: mesmo que op1=op1^op2
+
+//LOGICOS
+//!op1: verdadeiro se op1 for falso
+//op1 AND op2: verdadeiro se op1 e op2 forem verdadeiros
+//op1 OR op2: verdadeiro se op1 ou op2 forem verdadeiros
+//op1 XOR op2: verdadeiro se só op1 ou só op2 for verdadeiro
+//op1 && op2: verdadeiro se op1 e op2 forem verdadeiros (tem precedencia maior que AND)
+//op1 || op2: verdadeiro se op1 ou op2 forem verdadeiros (tem precedencia maior que OR)
+
+?>
