c119a4d8aaa5d651f2c0ccf7a90533073e6d2987
Nalu <pordosol699@gmail.com>
2025-07-15 11:13:04 -0300

    comando de repetição while

---
+++ PHP/repeticaoWhile.php
@@ -0,0 +1,16 @@
+<?php
+//while avalia a expressão e enquanto ela for verdadeira a excução do bloco de comandos em questão será repetida
+//sintaxe:
+//while(expressao)
+//{
+//comandos
+//}
+
+$cont = 1;
+while ($cont<100){
+    echo"O valor atual do contador é $cont <br>";
+    $cont++;
+}
+
+//quando cont for igual a 99 o comando while parara
+?>
