209f536cabfa3f691358e5565f2eba040f740d88
Nalu <pordosol699@gmail.com>
2025-07-15 10:06:21 -0300

    exemplo de variaveis sendo aplicadas em uma string

---
+++ PHP/variaveis.php
@@ -0,0 +1,11 @@
+<?php
+//no php não é necessário declarar o tipo de variavel, as variaveis devem iniciar com $, não pode começar com numero
+//ex de variaveis validas:
+// $joao23
+// $casal20
+// $teste450
+
+$time = "Grêmio";
+$titulo = "Campeão do mundo"
+echo "O $time é $titulo"
+?>
