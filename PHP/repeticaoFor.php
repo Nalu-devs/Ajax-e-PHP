754baf0e9aa5b55afa6c8ecec2c9420d623dc677
Nalu <pordosol699@gmail.com>
2025-07-15 11:20:41 -0300

    comando de repetição for

---
+++ PHP/repeticaoFor.php
@@ -0,0 +1,13 @@
+<?php
+//o for é usado quando queremos executar um conjunto de instruções um numero de vezes, sintaxe:
+//for(incio; condição; operador){
+//comando
+//}
+
+for($cont=0; $cont<10; $cont++){
+    echo"A variavel \$cont vale $cont";
+    echo"<br>";
+}
+
+//resultado A variavel $cont vale 0; até valer 9
+?>
