aa97e53b980df2a5514f435302458b44bf32714b
Nalu <pordosol699@gmail.com>
2025-07-18 11:57:07 -0300

    etreminar depois

---
+++ tratar formularios web/exemplo4.js
@@ -0,0 +1,24 @@
+window.onload = function (){
+    var estado = document.getElementById("estado");
+    estado.onblur = function(){ exbirCidades(estado);}
+}
+
+function exbirCidades(estado){
+    if(estado == null){return;}
+    var opcoes = estado.options;
+    var lista = "";
+    var selecionados = new Array();
+    var i =0, j=0, k=0;
+
+    for(i=0; i<opcoes.length; i++)
+        if (opcoes[i].selected){
+            selecionado[j]=opcoes[i].value;
+            j++;
+        }
+    for (k=0; k<selecionados.length; k++){
+        if(k<selecionados.length-1){lista +=selecionados[k]+",";}
+        else{ lista += selecionados[k];}
+    }
+    var url= "exemplo4.php?estados="+encodeURIComponent(lista);
+    requisicaoHTTP("GET", url, true);
+}
