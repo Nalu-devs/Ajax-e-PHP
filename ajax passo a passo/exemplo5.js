67acf81700753564afa080ecfaec804115531075
Nalu <pordosol699@gmail.com>
2025-07-18 10:11:52 -0300

    mostrando outras maneiras de pegar cabeçalhos especificos

--- ajax passo a passo/exemplo5.js
+++ ajax passo a passo/exemplo5.js
@@ -2,4 +2,7 @@ function trataDados(){
     var cabecalhos = ajax.getAllResponseHeaders();
     document.getElementById("saida").style.backgroundColor="pink";
     document.getElementById("saida").innerHTML="<pre>"+cabecalhos+"</pre>";
+    //caso quisesemos um cabeçalho especifico usariamos getResponseHeader
+    //var cabecalhos = ajax.getResponseHeader("Content-Type"); ou
+    //var cabecalhos = ajax.getResponseHeader("Content-Length");
 }
