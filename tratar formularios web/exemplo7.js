7cbf651c3d09641231f94aadc545e57b381f4547
Nalu <pordosol699@gmail.com>
2025-07-22 10:05:47 -0300

    gerando um novo grupo de checkbox

---
+++ tratar formularios web/exemplo7.js
@@ -0,0 +1,47 @@
+window.onload = function(){
+    var todosCampos = document.getElementsByTagName("input");
+    if(todosCampos != null){
+        for(var i=0; i<todosCampos.length; i++){
+            if(todosCampos[i].type=="radio"){
+                todosCampos[i].onclick=function(){
+                    mostraModelos(this)
+                }
+            }
+        }
+    }
+}
+
+function mostraModelos(marca){
+    if(marca == null){return}
+    var escolhida = marca.value;
+    var url = "exemplo7.php?marca="+encodeURIComponent(escolhida);
+    requisicaoHTTP("GET", url, true);
+}
+
+function trataDados(){
+    var info = ajax.responseText;
+    var arrayOpcoes = eval(info);
+    criaCaixas(arrayOpcoes);
+}
+
+function criaCaixas(opcoes){
+    var saida = document.getElementById("exibirAqui");
+    var nova;
+    
+    //remove todas checkboxes existentes
+    while(saida.hasChildNodes()){
+        for(var i=0; i<saida.childNodes.length; i++){
+            saida.removeChild(saida.firstChild);
+        }
+    }
+    //cria as checkboxes usando o array de opcoes
+    for(var i=0; i<opcoes.length; i++){
+        nova = document.createElement("input");
+        nova.setAttribute("type", "checkbox");
+        nova.setAttribute("name", "modelo");
+        nova.setAttribute("value", opcoes[i]);
+        saida.appendChild(nova);
+        saida.appendChild(document.createTextNode(opcoes[i]));
+        saida.appendChild(document.createElement("br"));
+    }
+}
