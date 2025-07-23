ee20681c4999a141efea077951d22312d9ae4ba4
Nalu <pordosol699@gmail.com>
2025-07-23 09:48:16 -0300

    atualizando

--- uma aplicação completa com ajax/loja/js/loja.js
+++ uma aplicação completa com ajax/loja/js/loja.js
@@ -1,39 +1,39 @@
-//carrega a pagina inicial
+// carrega a página inicial da loja
 window.onload = function(){
-    Loja('inicio', 0);
+	Loja('inicio', 0);
 }
 
-//envia a requisição ao servidor
+// envia a requisição ao servidor, de acordo com a ação do usuário
 function Loja(secao, parametro){
-    Aviso(1);//exibe o aguarde...
-    var url = "php/loja.php?"+secao+"="+encodeURIComponent(parametro);
-    requisicaoHTTP("GET", url, true);
+	Aviso(1);	// exibe o aviso "Aguarde..."
+	var url="php/loja.php?"+secao+"="+encodeURIComponent(parametro);
+	requisicaoHTTP("GET",url,true);
 }
 
-//envia a nova quantidade do produto, para atualização no carrinho de compras
+// envia a nova quantidade do produto, para atualização no carrinho de compras
 function AtualizaQuantidade(campo){
-    var id = campo.name;
-    var quant = campo.value;
-    Loja('quantidade', id+'-'+quant);
+	var id = campo.name;
+	var quant = campo.value;
+	Loja('quantidade',id+'-'+quant);	
 }
 
-//Exibe ou oculta a msg de espera
-function Aviso(exibir){
-    var saida = document.getElementById("avisos");
-    if(exibir){
-        saida.className = "aviso";
-        saida.innerHTML = "Aguarde...processando!";
-    }
-    else{
-        saida.className = "";
-        saida.innerHTML = "";
-    }
+// exibe ou oculta a mensagem de espera
+function Aviso(exibir) {
+	var saida = document.getElementById("avisos");
+	if(exibir){
+		saida.className = "aviso";
+		saida.innerHTML = "Aguarde...processando!";
+	}
+	else {
+		saida.className = "";
+		saida.innerHTML = "";
+	}
 }
 
-//exibe a resposta do servidor
+// exibe a resposta do servidor
 function trataDados(){
-    var info = ajax.responseText;
-    var saida = document.getElementById("conteudo");
-    saida.innerHTML = info;
-    Aviso(0);
+	var info = ajax.responseText;
+	var saida = document.getElementById("conteudo");
+	saida.innerHTML = info;
+	Aviso(0);
 }
