a4c8b7814098afc53adb06538a436b9b5a17d4d6
Nalu <pordosol699@gmail.com>
2025-07-25 09:46:43 -0300

    exercicio 3, em um array com 3 filmes adicionamos 2 no final e removemos o ultimo com push e pop

---
+++ exercicios de fixação/ex3.php
@@ -0,0 +1,15 @@
+<!-- Crie um array chamado $filmes com 3 nomes de filmes. Adicione mais dois filmes usando 
+array_push(). Depois, remova o último filme usando array_pop() e exiba o array final. -->
+
+<?php
+
+$filmes = array(
+    "Procurando Nemo",
+    "Enrolados",
+    "Hoje não vou embora sozinho"
+);
+
+array_push($filmes, "Sobrenatural", "Meninas Malvadas");
+array_pop($filmes);
+
+print_r($filmes);
