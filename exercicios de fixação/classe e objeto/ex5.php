cc26cc1f9475ea2a65dacd56569441d055bfcc81
Nalu <pordosol699@gmail.com>
2025-07-25 11:28:29 -0300

    exercicio 5, public static function somar($a, $b) Define um método estático, ou seja, você pode chamá-lo sem instanciar um objeto da classe. Calculadora::somar(10, 5) Chamada direta do método estático, passando os parâmetros necessários.

--- exercicios de fixação/classe e objeto/ex5.php
+++ exercicios de fixação/classe e objeto/ex5.php
@@ -1,2 +1,14 @@
 <!-- Crie uma classe Calculadora com um método estático somar($a, $b) que retorne a soma dos valores
 passados. Use esse método sem instanciar a classe. -->
+
+<?php
+
+class Calculadora{
+    public static function somar($a, $b){
+        return $a+$b;
+    }
+}
+
+echo Calculadora::somar(10, 10);
+
+?>
