65f20fcc45e61dd8cd620b59991ac1acfa52cf1d
Nalu <pordosol699@gmail.com>
2025-07-25 09:39:56 -0300

    exercicio 2 array sem ser associativo, para identificar se existe certa fruta no array

---
+++ exercicios de fixação/ex2.php
@@ -0,0 +1,16 @@
+<!-- Crie um array com os nomes de 5 frutas. Verifique se a fruta "banana" está presente no array.
+Se estiver, exiba a mensagem "Banana está na lista!". -->
+
+<?php
+
+$frutas = array(
+    "maça",
+    "banana",
+    "morango",
+    "amora",
+    "uva"
+);
+
+if(in_array("banana", $frutas)==TRUE){
+    echo "Banana esta no array: ".$fruta[1]
+}
