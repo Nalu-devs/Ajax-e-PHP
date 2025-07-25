09257c4c587b283b0b4c1bd3c43bd62e8442539e
Nalu <pordosol699@gmail.com>
2025-07-25 11:24:42 -0300

    exercicio 4, o que acontece se tentar acessar um atributo private diretamente fora da classe

--- exercicios de fixação/classe e objeto/ex4.php
+++ exercicios de fixação/classe e objeto/ex4.php
@@ -1,2 +1,22 @@
 <!-- Explique o que acontece se você tentar acessar um atributo private diretamente fora da classe.
 Faça um teste simples para comprovar. -->
+
+<?php
+
+//proteje os atributos da classe impedindo que seja acessado fora da classe
+
+class Retangulo{
+    private $largura;
+    private $altura;
+
+    public function calcularArea(){
+        echo "A area é igual a {$this->largura * $this->altura}";
+    }
+}
+
+$area = new Retangulo;
+$area->largura=2;//impossivel
+$area->altura=5;//impossivel
+
+$area->calcularArea();
+?>
