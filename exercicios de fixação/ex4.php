31adb5503b632f608f5424cae62acf13eb08a0fe
Nalu <pordosol699@gmail.com>
2025-07-25 09:50:19 -0300

    exercicio 4, para organizar os itens de um array de forma crescente

---
+++ exercicios de fixação/ex4.php
@@ -0,0 +1,16 @@
+<!-- Crie um array de números aleatórios: 32, 5, 7, 19, 11 Ordene esses números em ordem crescente 
+usando sort() Mostre o array antes e depois da ordenação. -->
+
+<?php
+$numeros = array(
+    32,
+    5,
+    7,
+    19,
+    11
+);
+
+print_r($numeros);
+sort($numeros);
+print_r($numeros);
+?>
