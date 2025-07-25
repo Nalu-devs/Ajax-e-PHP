172ae0759640703a1ee46a553c41f49cd7df04df
Nalu <pordosol699@gmail.com>
2025-07-25 11:18:18 -0300

    exercicio 3, tentando entender melhor classe e objeto

--- exercicios de fixação/classe e objeto/ex3.php
+++ exercicios de fixação/classe e objeto/ex3.php
@@ -2,3 +2,29 @@
 Crie métodos públicos setLargura(), setAltura(), getLargura(), getAltura()
 e um método calcularArea() que retorne a área do retângulo. Crie um objeto,
 defina valores e exiba a área. -->
+
+<?php
+
+class Retangulo{
+    private $largura;
+    private $altura;
+
+    public function setLargura($largura) {
+        $this->largura = $largura;
+    }
+
+    public function setAltura($altura) {
+        $this->altura = $altura;
+    }
+
+    public function calcularArea(){
+        echo "A area é igual a {$this->largura * $this->altura}";
+    }
+}
+
+$area = new Retangulo;
+$area->setLargura(2);
+$area->setAltura(5);
+
+$area->calcularArea();
+?>
