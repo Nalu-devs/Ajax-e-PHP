ab27afb5aae97e606c03136437df6fc19e20dd31
Nalu <pordosol699@gmail.com>
2025-07-25 09:24:31 -0300

    exercicio 1, criar um array onde no fim apareça somente um elemento do array

---
+++ exercicios de fixação/ex1.php
@@ -0,0 +1,12 @@
+<!-- Crie um array associativo chamado $aluno que contenha as chaves "nome", "idade" e "curso" 
+com valores fictícios. Depois, exiba o valor do curso na tela. -->
+
+<?php
+$aluno = array(
+    "nome" => "Ana Lúcia",
+    "idade" => 18,
+    "curso" => "Informática para Internet"
+);
+
+echo "Curso: " . $aluno["curso"];
+?>
