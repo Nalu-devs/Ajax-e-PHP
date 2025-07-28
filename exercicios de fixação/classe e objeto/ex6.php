b1f723eb1d8dca785775c473518c940657e082a2
Nalu <pordosol699@gmail.com>
2025-07-28 09:29:39 -0300

    exercicio 6, iniciamos uma classe animal, que tem o metodo falar, depois criamos uma filha que herda a classe animal a classe Cachorro, que substitui a função e sobrescreve falar

--- exercicios de fixação/classe e objeto/ex6.php
+++ exercicios de fixação/classe e objeto/ex6.php
@@ -1,3 +1,28 @@
 <!-- Crie uma classe Animal com um método falar(). Depois crie uma classe Cachorro que herda Animal 
 e sobrescreve o método falar() para imprimir “Au Au!”. Instancie um objeto Cachorro e chame 
 o método falar(). -->
+
+<?php
+
+class Animal{
+
+    public function falar(){
+        echo "Som do animal";
+    }
+}
+
+// Classe filha que herda de Animal
+class Cachorro extends Animal {
+    // Sobrescreve o método falar()
+    public function falar() {
+        echo "Au Au!";
+    }
+}
+
+// Instancia o objeto da classe Cachorro
+$meuCachorro = new Cachorro();
+
+// Chama o método falar()
+$meuCachorro->falar();
+
+?>
